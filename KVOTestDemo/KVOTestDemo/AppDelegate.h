49f4d0ed415d4960044a530ff891ba660dd10ee8
huanghualove <1736591423@qq.com>
2017-10-17 14:45:37 +0800

    test

---
+++ KVOTestDemo/KVOTestDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KVOTestDemo
+//
+//  Created by 黄华 on 2017/10/17.
+//  Copyright © 2017年 huanghua. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ KVOTestDemo/KVOTestDemoTests/KVOTestDemoTests.m
@@ -0,0 +1,39 @@
+//
+//  KVOTestDemoTests.m
+//  KVOTestDemoTests
+//
+//  Created by 黄华 on 2017/10/17.
+//  Copyright © 2017年 huanghua. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface KVOTestDemoTests : XCTestCase
+
+@end
+
+@implementation KVOTestDemoTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
